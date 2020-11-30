8b858abcfe1b72dedf000d919ac67a70b97b75a0
gunjan <pandya.gunjan@gmail.com>
2020-11-30 11:36:16 -0500

    done with 3 methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -15,48 +15,60 @@ public class ArrayUtility<T> {
 
   public  Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate)
   {
-      Integer count = 0;
-      for (T object : array) {
-          if (object.equals(valueToEvaluate)){
-              count++;
-          }
-      }
-      for (T object : arrayToMerge) {
-          if (object.equals(valueToEvaluate)) {
-              count++;
-          }
-      }
-      return count;
+//      Integer count = 0;
+//      for (T object : array) {
+//          if (object.equals(valueToEvaluate)){
+//              count++;
+//          }
+//      }
+//      for (T object : arrayToMerge) {
+//          if (object.equals(valueToEvaluate)) {
+//              count++;
+//          }
+//      }
+//      return count;
+      List<T> valueList = new ArrayList<>();
+      valueList.addAll(Arrays.asList(arrayToMerge));
+      valueList.addAll(Arrays.asList(array));
+      return Collections.frequency(valueList, valueToEvaluate);
   }
 
 
-//    public  T[] removeValue(T valueToRemove)
-//    {
-//
-//        ArrayList<T> storeAltered= (ArrayList<T>) Arrays.asList(array);
-//
-//        for (T object : array)
-//        {
-//            if(object.equals(valueToRemove))
-//            {
-//                storeAltered.remove(object);
-//            }
-//
-//        }
-//        T[] finalArray = storeAltered.toArray();
-//        return finalArray;
-//    }
+    public  T[] removeValue(T valueToRemove)
+    {
+
+       Integer occuranceCount= getNumberOfOccurrences(valueToRemove);
+       T[] buffer=Arrays.copyOf(array,array.length-occuranceCount);
+       int j=0;
+        for (int i = 0; i < array.length; i++) {
+
+            if(!array[i].equals(valueToRemove))
+            {
+                buffer[j]=array[i];
+                j++;
+            }
+
+        }
+        return buffer;
+
+    }
 
     public  Integer getNumberOfOccurrences(T valueToEvaluate)
     {
+
+//        ArrayList<T>  valueList = new ArrayList<>();
+//        valueList.addAll(Arrays.asList(array));
+//        return Collections.frequency(valueList, valueToEvaluate);
         Integer count = 0;
         for (T object : array) {
             if (object.equals(valueToEvaluate)){
                 count++;
             }
         }
-
         return count;
+
+
+
     }
 
 
