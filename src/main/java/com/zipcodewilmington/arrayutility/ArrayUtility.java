f60f5bf5cd521d102230667f5926fa28cade73a1
gunjan <pandya.gunjan@gmail.com>
2020-11-30 11:00:22 -0500

    done with first method for CountDuplicate

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,32 @@
 package com.zipcodewilmington.arrayutility;
 
+import javax.swing.*;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] array;
+
+    public ArrayUtility(T[] inputArray) {
+        this.array=inputArray;
+    }
+
+  public  Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate)
+  {
+      Integer count = 0;
+      for (T object : array) {
+          if (object.equals(valueToEvaluate)){
+              count++;
+          }
+      }
+      for (T object : arrayToMerge) {
+          if (object.equals(valueToEvaluate)) {
+              count++;
+          }
+      }
+      return count;
+  }
+
 }
